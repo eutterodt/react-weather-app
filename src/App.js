f4aa6ca141c2f3eb6b40834ef0a7c273d0934124
eutterodt <e.utterodt@gmail.com>
2023-01-15 23:17:44 +0100

    added API Calls for the weather App

--- src/App.js
+++ src/App.js
@@ -1,77 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-import cloud from "./media/cloud.png";
 import axios from "axios";
+import WeatherContent from "./WeatherContent";
 
 export default function WeatherApp() {
-  let apiKey = "ddf91e7f98ae542cfe124eef0bb0b9fb";
-  let city = "London";
-  let apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${city}&appid=${apiKey}&units=metric`;
-  return (
-    <div className="outer-wrapper">
-      <div className="container">
-        <div className="wrapper">
-          <div className="head">
-            <h1>Weather Application</h1>
-            <form className="search-form" id="search">
-              <input
-                className="inputfield"
-                id="city-search-input"
-                type="text"
-                placeholder="City Name"
-              />
-              <input
-                className="submitbutton"
-                id="submit"
-                type="submit"
-                value="Search"
-              />
-            </form>
-          </div>
-          <div className="h2">
-            <h2 id="searched-city">New York</h2>
-          </div>
-          <div className="row">
-            <div className="col-7">
-              <ul>
-                <li id="current-date">Sunday January 15th</li>
-                <li id="current-time">15:02 o clock</li>
-                <li id="weather-condition">Clear sky</li>
-              </ul>
-              <img className="weathericon" id="weathericon" src={cloud} alt="" />
-              <span className="displaytemperature" id="displaytemp">
-                6
-      </span>
-              <span className="unit">°C</span>
+  const [city, setCity] = useState("New York");
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  function handleResponse(response) {
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      date: new Date(response.data.dt * 1000),
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      city: response.data.name,
+      description: response.data.weather[0].description,
+      image: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function search() {
+    let apiKey = "ddf91e7f98ae542cfe124eef0bb0b9fb";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    search(city);
+
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="outer-wrapper">
+        <div className="container">
+          <div className="wrapper">
+            <div className="head">
+              <h1>Weather Application</h1>
+              <form className="search-form" id="search" onSubmit={handleSubmit}>
+                <input
+                  className="inputfield"
+                  id="city-search-input"
+                  type="text"
+                  placeholder="City Name"
+                  onChange={handleCityChange}
+                />
+                <input
+                  className="submitbutton"
+                  id="submit"
+                  type="submit"
+                  value="Search"
+                />
+              </form>
             </div>
-            <div className="col-5">
-              <ul>
-                <li className="weatherstats">
-                  Precipitation: <span id="precipitation">15</span>%
-        </li>
-                <li className="weatherstats">
-                  Humidity: <span id="humidity">86</span>%
-        </li>
-                <li className="weatherstats">
-                  Wind: <span id="wind">2</span> km/h
-        </li>
-              </ul>
-            </div>
+            <WeatherContent data={weatherData} />
+
           </div>
-        </div>
-        <small>
-          Coded by Elisa Utterodt {" "}
-          <a
-            className="github-link"
-            href="https://github.com/eutterodt/react-weather-app"
-            target="blank"
-            rel="noreferrer"
-            aria-label="link to github"
-          >
-            view open source code on github
+          <small>
+            Coded by Elisa Utterodt {" "}
+            <a
+              className="github-link"
+              href="https://github.com/eutterodt/react-weather-app"
+              target="blank"
+              rel="noreferrer"
+              aria-label="link to github"
+            >
+              view open source code on github
       </a>
-        </small>
+          </small>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    search();
+    return "Loading …"
+  }
 }
---
+++ src/WeatherContent.js
@@ -0,0 +1,36 @@
+import React from "react";
+import axios from "axios";
+import FormattedDate from "./FormattedDate";
+
+export default function WeatherContent(props) {
+  return (
+    <div className="weathercontent">
+      <div className="h2">
+        <h2 id="searched-city">{props.data.city}</h2>
+      </div>
+      <div className="row">
+        <div className="col-7">
+          <ul>
+            <li id="current-date"><FormattedDate date={props.data.date} /></li>
+            <li id="weather-condition">{props.data.description}</li>
+          </ul>
+          <img className="weathericon" id="weathericon" src={props.data.image} alt={props.data.description} />
+          <span className="displaytemperature" id="displaytemp">
+            {Math.round(props.data.temperature)}
+          </span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-5">
+          <ul>
+            <li className="weatherstats">
+              Humidity: <span id="humidity">{props.data.humidity}</span>%
+        </li>
+            <li className="weatherstats">
+              Wind: <span id="wind">{Math.round(props.data.wind)}</span> km/h
+        </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
